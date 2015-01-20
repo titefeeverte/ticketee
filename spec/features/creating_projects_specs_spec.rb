7bdea52e514b62619a1287df3f28b25697db5687
titefeeverte <titefeeverte@yahoo.fr>
2015-01-20 10:46:08 +0100

    all changes from 1 week

--- spec/features/creating_projects_specs_spec.rb
+++ spec/features/creating_projects_specs_spec.rb
@@ -7,8 +7,10 @@ feature "CreatingProjects", :type => :feature do
   	visit '/'
 
   	click_link 'New Project'
+   
 
-  	fill_in 'name' , with: 'TextMate 2'
+  	page.fill_in 'name' , with: 'TextMate 2'
+
   	fill_in 'description' , with: 'A text-editor for OS X'
   	click_button 'Create Project'
   	expect(page).to have_content('Project has been created.')
