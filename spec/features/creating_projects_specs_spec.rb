b738000abbd864ad09b80a09f7107c51082d4757
titefeeverte <titefeeverte@yahoo.fr>
2015-01-20 14:46:54 +0100

    creating rspec test

--- spec/features/creating_projects_specs_spec.rb
+++ spec/features/creating_projects_specs_spec.rb
@@ -12,7 +12,7 @@ feature "CreatingProjects", :type => :feature do
   	page.fill_in 'project[name]' , with: 'TextMate 2'
 
   	fill_in 'project[description]' , with: 'A text-editor for OS X'
-  	click_button 'Create Project'
+  	click_button('Create')
   	expect(page).to have_content('Project has been created.')
   end
 end
