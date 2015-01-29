6a0b4cbdf4343c6a452d44b7de52f0f184eda437
titefeeverte <titefeeverte@yahoo.fr>
2015-01-29 15:06:03 +0100

    Implement displaying a list o relevant tickets.

--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -17,20 +17,20 @@ feature "Creating Tickets" do
 	end
 
 
-    scenario "Creating a ticket without valid attributes fails" do
-   		click_button "Create Ticket"
-    	expect(page).to have_content("Ticket has not been created.")
-    	expect(page).to have_content('Title can\'t be blank')
-    	expect(page).to have_content("Description can't be blank")
-  end
+	scenario "Creating a ticket without valid attributes fails" do
+		click_button "Create Ticket"
+		expect(page).to have_content("Ticket has not been created.")
+		expect(page).to have_content('Title can\'t be blank')
+		expect(page).to have_content("Description can't be blank")
+	end
 	
 	scenario 'Description must be longer than 10 characters' do
-    fill_in 'title', with: 'Non-standards compliance'
-    fill_in 'description', with: 'it sucks'
-    click_button 'Create Ticket'
-    
-    expect(page).to have_content('Ticket has not been created.')
-    expect(page).to have_content('Description is too short')
-  end
-  
+		fill_in 'title', with: 'Non-standards compliance'
+		fill_in 'description', with: 'it sucks'
+		click_button 'Create Ticket'
+
+		expect(page).to have_content('Ticket has not been created.')
+		expect(page).to have_content('Description is too short')
+	end
+
 end
