6aba1d7bf31388ac54b921e12104c310f108fdf2
titefeeverte <titefeeverte@yahoo.fr>
2015-01-29 13:52:45 +0100

    creating tickets

---
+++ db/migrate/20150127115440_create_tickets.rb
@@ -0,0 +1,13 @@
+class CreateTickets < ActiveRecord::Migration
+  def change
+    create_table :tickets do |t|
+      t.string :title
+      t.text :description
+      t.string :∖
+      t.references :project, index: true
+
+      t.timestamps null: false
+    end
+    add_foreign_key :tickets, :projects
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150113152242) do
+ActiveRecord::Schema.define(version: 20150127115440) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name",        limit: 255
@@ -20,4 +20,16 @@ ActiveRecord::Schema.define(version: 20150113152242) do
     t.datetime "updated_at"
   end
 
+  create_table "tickets", force: :cascade do |t|
+    t.string   "title",       limit: 255
+    t.text     "description", limit: 65535
+    t.string   "∖",           limit: 255
+    t.integer  "project_id",  limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  add_index "tickets", ["project_id"], name: "index_tickets_on_project_id", using: :btree
+
+  add_foreign_key "tickets", "projects"
 end
