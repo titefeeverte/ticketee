957b9c68d8cf78075ac8b6b04727950da56aad7f
titefeeverte <titefeeverte@yahoo.fr>
2015-02-04 10:01:00 +0100

    test de différents link dans application.html.erb pour résoudre error Rspec routing

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -4,8 +4,8 @@ RSpec.feature "A user can sign in" do
   scenario "with valid credentials" do
     visit "/"
     click_link "Sign in"
-    fill_in "Email", with: user.email
-    fill_in "Password", with: "password"
+    fill_in "user[email]", with: user.email
+    fill_in "user[password]", with: "password"
     click_button "Log in"
     expect(page).to have_content("Signed in successfully.")
     expect(page).to have_content("Signed in as #{user.email}")
