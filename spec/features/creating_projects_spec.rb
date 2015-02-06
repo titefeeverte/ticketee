8ba7f3617756e6b091dd873b5ed70cf29d261e6e
Denis Lamotte <denis.lamotte@gmail.com>
2015-02-06 11:30:15 +0100

    update test to use before- test still failing

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -1,21 +1,23 @@
 require 'rails_helper'
-before do
-  sign_in_as!(FactoryGirl.create(:admin_user))
+require "rspec/expectations"
+
 
 feature 'CreatingProjects', :type => :feature do
+  before do
+    sign_in_as!(FactoryGirl.create(:admin_user))
+  end
+
   #pending "add some scenarios (or delete) #{__FILE__}"
   scenario 'can create a project' do
-  	
+
   	visit '/'
 
   	click_link 'New Project'
-   
 
-  	page.fill_in 'project[name]' , with: 'TextMate 2'
-
+  	fill_in 'project[name]' , with: 'TextMate 2'
   	fill_in 'project[description]' , with: 'A text-editor for OS X'
   	click_button('Create')
   	expect(page).to have_content('Project has been created.')
-    end
+
   end
 end
